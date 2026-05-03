b76fb46721839ef210700ee6d00763f5646c30af
Reuben Thomas <rrt@sc3d.org>
2026-05-03 23:49:04 +0100

    Bump version to 1.3.12 and bump copyright years

--- ursa-ts-mode.el
+++ ursa-ts-mode.el
@@ -1,12 +1,12 @@
 ;;; ursa-ts-mode.el --- Major mode for Ursa, using tree-sitter  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2023 Free Software Foundation, Inc.
-;; Copyright (C) 2023-2024 Reuben Thomas <rrt@sc3d.org>
+;; Copyright (C) 2023-2026 Reuben Thomas <rrt@sc3d.org>
 
 ;; Author     : Reuben Thomas <rrt@sc3d.org>
 ;; Maintainer : Reuben Thomas <rrt@sc3d.org>
 ;; Created    : August 2023
-;; Version    : 1.3.11
+;; Version    : 1.3.12
 ;; Keywords   : ursalang, languages, tree-sitter
 ;; Package-Requires : ((emacs "29.1"))
 ;; URL: https://github.com/ursalang/ursa-ts-mode
